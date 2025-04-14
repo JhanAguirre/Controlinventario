319e639df2a12aa1a882cde045f01001268f1ed9
JhanAguirre <jhanalejandroaguirre5@gmail.com>
2025-04-14 01:24:13 -0500

    Create Proveedor

---
+++ resources/views/Proveedores/create.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Agregar Proveedor</h1>
+    <form action="{{ route('proveedores.store') }}" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="nombre">Nombre</label>
+            <input type="text" class="form-control" id="nombre" name="nombre" required>
+        </div>
+        <div class="form-group">
+            <label for="direccion">Dirección</label>
+            <input type="text" class="form-control" id="direccion" name="direccion" required>
+        </div>
+        <div class="form-group">
+            <label for="telefono">Teléfono</label>
+            <input type="text" class="form-control" id="telefono" name="telefono" required>
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" name="email" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Guardar</button>
+    </form>
+</div>
+@endsection
